163b3662f8dfebd94d815b16d428dae7ebef421c
Giovana Zuffo <giovana_zuffo@estudante.sesisenai.org.br>
2025-10-16 16:41:06 -0300

    mm

--- js/js.js
+++ js/js.js
@@ -1,12 +1,15 @@
 let resposta = document.getElementById('resposta')
 
+function calcularGeracao(potencia, irradiacao, efici){
+return (potencia * irradiacao * 30 * efici) / 1000
+}
+
 function principal(){
     let potencia = Number(document.getElementById('potencia').value)
     let irradiacao = Number(document.getElementById('irradiacao').value)
-    let dias = Number(document.getElementById('dias').value)
     let efici = Number(document.getElementById('efici').value) / 100
 
-    let final = (potencia * irradiacao * dias * efici) / 1000
+    let final = calcularGeracao(potencia, irradiacao, efici)
 
     console.log(`A geração mensal de energia é: ${final.toFixed(2)}`)
 
